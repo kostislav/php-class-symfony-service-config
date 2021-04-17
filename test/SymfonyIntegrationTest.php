e88aa9d0cd87fbf2e7e99c7d9e7f935338c85fbe
Jan Judas <jan@judas.cz>
2021-04-17 09:44:54 +0200

    simplify tests

--- test/SymfonyIntegrationTest.php
+++ test/SymfonyIntegrationTest.php
@@ -2,37 +2,41 @@
 
 use ConfigClasses\Valid\CombinedServiceConfig;
 use ConfigClasses\Valid\SimpleConfig;
+use Kostislav\ClassConfig\ConfigClassServiceConfigLoader;
 use PHPUnit\Framework\TestCase;
-use Spatie\TemporaryDirectory\TemporaryDirectory;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use function PHPUnit\Framework\assertThat;
 use function PHPUnit\Framework\equalTo;
 
 include __DIR__ . '/../vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
 
 class SymfonyIntegrationTest extends TestCase {
-    private $tempDir;
-
-    protected function setUp(): void {
-        $this->tempDir = (new TemporaryDirectory())->create();
-    }
-
-    protected function tearDown(): void {
-        $this->tempDir->delete();
-    }
-
     /** @test */
     function createsSimpleController() {
-        $kernel = new TestKernel($this->tempDir->path(), [SimpleConfig::class]);
-        $service = $kernel->getService('someService');
+        $container = $this->buildContainer([SimpleConfig::class]);
+
+        $service = $container->get('someService');
 
         assertThat($service->value(), equalTo('sesd'));
     }
 
     /** @test */
     function injectsOtherDefinedServices() {
-        $kernel = new TestKernel($this->tempDir->path(), [CombinedServiceConfig::class]);
-        $service = $kernel->getService('outerService');
+        $container = $this->buildContainer([CombinedServiceConfig::class]);
+
+        $service = $container->get('outerService');
 
         assertThat($service->combinedValue(), equalTo('sesd dada'));
+    }
+
+    private function buildContainer(array $configClasses): Container {
+        $containerBuilder = new ContainerBuilder();
+        $loader = new ConfigClassServiceConfigLoader($containerBuilder);
+        foreach ($configClasses as $configClass) {
+            $loader->load($configClass);
+        }
+        $containerBuilder->compile();
+        return $containerBuilder;
     }
 }
